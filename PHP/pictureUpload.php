29aa92276ad6dba8dc2bb36a25af8ed091511676
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-03-01 21:13:21 +0600

    added

---
+++ PHP/pictureUpload.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image upload</title>
+</head>
+<body>
+    <form action="post">
+    <input type="file" name="myfile" id="">
+    <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
